74cc4b918312313110208b3dfb1cad0e2a81c696
Zakiah Aulia <muhammadhafizudin1212@gmail.com>
2025-09-03 15:25:00 +0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+   <>
+   <h1 className="text-5xl text-center p-8 bg-indigo-400 text-white">Kita mulai dari sini</h1>
+   </>
+  );
+}
